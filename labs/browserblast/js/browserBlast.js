15962f8b1639ff89a4ec57d1e83b5392bcb451b2
Mark Goodyear <hello@markgoodyear.com>
2013-07-30 22:10:19 +0100

    Automated commit at 2013-07-30 21:10:19 UTC by middleman-deploy 3.1.4

--- labs/browserblast/js/browserBlast.js
+++ labs/browserblast/js/browserBlast.js
@@ -1,10 +1,1 @@
-/*------------------------------------------------------------
-
-	browserBlast v2.0.0
-	Author: Mark Goodyear - http://www.markgoodyear.com
-	Git: https://github.com/markgoodyear/browserblast
-	Email: hello@markgoodyear.com
-	Twitter: @markgdyr
-
-------------------------------------------------------------*/
-var browserBlast=function(e){"use strict";function s(e){var t=document.createElement("div");t.id="browserblast",t.style.zIndex="2147483647",t.innerHTML=i,document.body.appendChild(t),document.documentElement.className+=" unsupported-browser"}function o(){var e=-1;if(navigator.appName==="Microsoft Internet Explorer"){var t=navigator.userAgent,n=new RegExp("MSIE ([0-9]{1,}[.0-9]{0,})");n.exec(t)!==null&&(e=parseFloat(RegExp.$1))}return e}var t=e||{},n=t.devMode||!1,r=t.supportedIE||"8",i=t.message||"Hey! Your browser is unsupported. Please <a href='http://browsehappy.com' target='_blank'>upgrade</a> for the best experience.",u=o();(u>-1&&u<r+".0"||n===!0)&&s()};
+var browserBlast=function(e){"use strict";function r(){var e=document.createElement("div");e.id="browserblast",e.style.zIndex="2147483647",e.innerHTML=s,document.body.appendChild(e),document.documentElement.className+=" unsupported-browser"}function t(){var e=-1;if("Microsoft Internet Explorer"===navigator.appName){var r=navigator.userAgent,t=new RegExp("MSIE ([0-9]{1,}[.0-9]{0,})");null!==t.exec(r)&&(e=parseFloat(RegExp.$1))}return e}var n=e||{},a=n.devMode||!1,o=n.supportedIE||"8",s=n.message||"Hey! Your browser is unsupported. Please <a href='http://browsehappy.com' target='_blank'>upgrade</a> for the best experience.",p=t();(p>-1&&o+".0">p||a===!0)&&r()};
